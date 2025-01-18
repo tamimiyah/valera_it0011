966ece5e25efc009fb5bbf813e520511220ef8d7
tamimiyah <160693673+tamimiyah@users.noreply.github.com>
2025-01-18 19:59:43 +0800

    FA1

---
+++ Formative Technicals/Valera_FA1.py
@@ -0,0 +1,20 @@
+print("****************************************")
+print("\t  PERSONAL INFORMATION")
+print("****************************************")
+
+firstName= input(" Enter First Name: ")
+middleName= input("Enter Middle Name: ")
+lastName= input("  Enter Last Name: ")
+birthday= input("   Enter Birthday: ")
+gender= input ("     Enter Gender: ")
+address= input ("    Enter Address: ")
+course= input("     Enter Course: ")
+yearLevel= input(" Enter Year Level: ")
+
+
+print("\n  Full Name: ", firstName, middleName, lastName)
+print("   Birthday: ", birthday)
+print("     Gender: ", gender)
+print("    Address: ", address)
+print("     Course: ", course)
+print(" Year Level: ", yearLevel)
