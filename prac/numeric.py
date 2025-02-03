7e2d0c1d672f958140a735f316a37b844012e59b
tamimiyah <202312467@fit.edu.ph>
2025-02-03 15:19:57 +0800

    03/02/2025_3:19PM_TSA1

---
+++ prac/numeric.py
@@ -0,0 +1,43 @@
+x = 159
+y = 25
+
+print (x * y)
+
+print (x / y)
+
+print (x // y)
+
+print (divmod(x, y))
+
+print(not(x < 5 and y > 5))
+
+#Identity Operators
+
+x = 1
+y = 1
+
+print(x is y)
+
+x = 1
+y = x
+
+print(x is y)
+
+x = 1
+y = 2
+
+print(x is y)
+
+
+#Membership Operators
+
+x = [1, 2, 3, 4, 5]
+y = 3
+
+print(y in x)
+
+x = {'a': 1, 'b': 2}
+y = 'c'
+
+print(y in x)
+
